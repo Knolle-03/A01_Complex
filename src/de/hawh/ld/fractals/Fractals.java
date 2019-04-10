dc95e8c4c3b28cd83542239ed5bd8d0ff4192794
Knolle-03 <Knolle-03@users.noreply.github.com>
2019-04-10 15:24:24 +0200

    comments

--- src/de/hawh/ld/fractals/Fractals.java
+++ src/de/hawh/ld/fractals/Fractals.java
@@ -18,10 +18,9 @@ public class Fractals {
         Complex c = new ComplexImmutable(0.4,-0.325);
 
         // use Complex a for mandelbrotSet
-        ArrayList<String> list1  = (Fractals.mandelbrotSet(a, 1000));
+        ArrayList<String> list1  = Fractals.complexToString(Fractals.mandelbrotSet(a, 1000));
         // use Complex z and c for juliaSet
-        ArrayList<String> list2  = (Fractals.juliaSet(z, c, 999));
-
+        ArrayList<String> list2  = (Fractals.complexToString(Fractals.juliaSet(z, c, 1000)));
         // write both to files
         FileIO.fileWriter(list1, "mandelbrotSet(0.1 +0.1i).txt");
         FileIO.fileWriter(list2, "juliaSet z = (0.3 +0.01i) c = (0.4 -0.325i).txt");
@@ -46,56 +45,68 @@ public class Fractals {
 
         System.out.println("-------------------------------------------------");
 
-        ArrayList<String> sortedList = Fractals.sortList(juliaObjs);
+        ArrayList<String> sortedList = Fractals.doubleToString(Fractals.sortList(juliaObjs));
         System.out.println(sortedList);
         FileIO.fileWriter(sortedList, "sortedJuliaList.txt");
 
     }
 
 
-    private static ArrayList<String> mandelbrotSet(Complex num, int depth) {
+    static ArrayList<Complex> mandelbrotSet(Complex num, int depth) {
         Complex z = new ComplexImmutable();
 
-        return new ArrayList<>(Fractals.calcSet(z,num,depth));
+        return Fractals.calcSet(z,num,depth);
     }
 
+    static ArrayList<Complex> juliaSet(Complex z, Complex c, int depth) {
 
-    private static ArrayList<String> juliaSet(Complex z, Complex c, int depth) {
-        ArrayList<String> juliaSet = new ArrayList<>();
-        juliaSet.add(z.toString());
-        juliaSet.addAll(Fractals.calcSet(z,c,depth));
-
-        return juliaSet;
+        return Fractals.calcSet(z,c,depth);
     }
 
-    private static ArrayList<String> calcSet(Complex z, Complex c, int depth) {
-        ArrayList<String> set = new ArrayList<>();
+    private static ArrayList<Complex> calcSet(Complex z, Complex c, int depth) {
+        ArrayList<Complex> set = new ArrayList<>();
 
         for(int i = 0; i < depth; i++) {
             z = MathUtils.add(MathUtils.powerOfTwo(z), c);
-            set.add(z.toString());
+            set.add(z);
         }
 
         return set;
     }
 
-    private static ArrayList<String> sortList(ArrayList<Complex> list) {
+    static ArrayList<String> complexToString(ArrayList<Complex> set) {
+        ArrayList<String> stringSet = new ArrayList<>();
+
+        for(Complex c: set) {
+            stringSet.add(c.toString());
+        }
+
+        return stringSet;
+    }
+
+    static ArrayList<String> doubleToString(ArrayList<Double> set) {
+        ArrayList<String> stringSet = new ArrayList<>();
+
+        for(Double c: set) {
+            stringSet.add(c.toString());
+        }
+
+        return stringSet;
+    }
+
+
+    static ArrayList<Double> sortList(ArrayList<Complex> list) {
         ArrayList<Double> absList = new ArrayList<>();
-        ArrayList<String> absListString = new ArrayList<>();
 
         for(Complex obj: list) {
             absList.add(MathUtils.getAbs(obj));
         }
         absList.sort(naturalOrder());
 
-        for(Double abs: absList) {
-            absListString.add(Double.toString(abs));
-        }
-
-        return absListString;
+        return absList;
     }
 
-    private static ArrayList<Complex> generateSet(ArrayList<String> list ) {
+    static ArrayList<Complex> generateSet(ArrayList<String> list ) {
         ArrayList<Complex> set = new ArrayList<>();
 
         for(String str: list) {
